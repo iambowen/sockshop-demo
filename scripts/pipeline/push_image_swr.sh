6601192237a20ddd60ccca2288ca2f8d49a03472
Mohammad Asif Siddiqui <mohammad.asif.siddiqui1@huawei.com>
2018-02-01 22:50:12 +0530

    Create push_image_swr.sh

---
+++ scripts/pipeline/push_image_swr.sh
@@ -0,0 +1,8 @@
+docker push registry.cn-north-1.huaweicloud.com/bigheadbird/sockshop-catalogue:latest
+docker push registry.cn-north-1.huaweicloud.com/bigheadbird/sockshop-payment:latest
+docker push registry.cn-north-1.huaweicloud.com/bigheadbird/sockshop-user:latest
+docker push registry.cn-north-1.huaweicloud.com/bigheadbird/sockshop-front-end:latest
+docker push registry.cn-north-1.huaweicloud.com/bigheadbird/sockshop-carts:latest
+docker push registry.cn-north-1.huaweicloud.com/bigheadbird/sockshop-shipping:latest
+docker push registry.cn-north-1.huaweicloud.com/bigheadbird/sockshop-orders:latest
+docker push registry.cn-north-1.huaweicloud.com/bigheadbird/sockshop-mesher:latest
