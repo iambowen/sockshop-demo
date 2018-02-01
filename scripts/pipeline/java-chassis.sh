cf0e4eed8f033dcfbc1d4d5240962517437b3e71
Mohammad Asif Siddiqui <mohammad.asif.siddiqui1@huawei.com>
2018-02-01 20:31:55 +0530

    Update java-chassis.sh

--- scripts/pipeline/java-chassis.sh
+++ scripts/pipeline/java-chassis.sh
@@ -22,8 +22,8 @@ cp ./carts/target/carts.jar ./build_image_carts
 cp ./makedocker/carts/Dockerfile ./build_image_carts
 cp ./makedocker/carts/carts.sh ./build_image_carts
 cd build_image_carts
-sudo docker build -t carts:autobuild .
-sudo docker tag carts:autobuild registry.cn-north-1.huaweicloud.com/bigheadbird/sockshop-carts:latest
+docker build -t carts:autobuild .
+docker tag carts:autobuild registry.cn-north-1.huaweicloud.com/bigheadbird/sockshop-carts:latest
 cd ..
 
 #orders
@@ -32,8 +32,8 @@ cp ./orders/target/orders.jar ./build_image_orders
 cp ./makedocker/orders/Dockerfile ./build_image_orders
 cp ./makedocker/orders/orders.sh ./build_image_orders
 cd build_image_orders
-sudo docker build -t orders:autobuild .
-sudo docker tag orders:autobuild registry.cn-north-1.huaweicloud.com/bigheadbird/sockshop-orders:latest
+docker build -t orders:autobuild .
+docker tag orders:autobuild registry.cn-north-1.huaweicloud.com/bigheadbird/sockshop-orders:latest
 cd ..
 
 #shipping
@@ -42,6 +42,6 @@ cp ./shipping/target/shipping.jar ./build_image_shipping
 cp ./makedocker/shipping/Dockerfile ./build_image_shipping
 cp ./makedocker/shipping/shipping.sh ./build_image_shipping
 cd build_image_shipping
-sudo docker build -t shipping:autobuild .
-sudo docker tag shipping:autobuild registry.cn-north-1.huaweicloud.com/bigheadbird/sockshop-shipping:latest
+docker build -t shipping:autobuild .
+docker tag shipping:autobuild registry.cn-north-1.huaweicloud.com/bigheadbird/sockshop-shipping:latest
 cd ..
