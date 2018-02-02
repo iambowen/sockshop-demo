09f269a82e6e8a927cb94e1489938d9c7a9875a7
savitaashture <savita@huawei.com>
2018-02-02 19:10:59 +0530

    Update mesher.sh

--- scripts/pipeline/mesher.sh
+++ scripts/pipeline/mesher.sh
@@ -2,10 +2,7 @@ cd pwd
 
 #frontend
 mkdir build_image_frontend
-cp ./front-end/Dockerfile ./build_image_frontend
-cp ./front-end/package.json ./build_image_frontend
-cp ./front-end/yarn.lock ./build_image_frontend
-cp ./front-end/server.js ./build_image_frontend
+cp -r ./front-end/* ./build_image_frontend/
 cd build_image_frontend
 docker build -t frontend:autobuild .
 docker tag frontend:autobuild registry.cn-north-1.huaweicloud.com/hwcse/sockshop-frontend:latest
