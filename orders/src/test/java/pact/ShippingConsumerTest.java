cb0bf20c618e1b78bc4a6cc5277521a7075ac34f
Bowen Ma <iambowen.m@gmail.com>
2018-03-13 00:32:21 +0800

    import an consumer client

--- orders/src/test/java/pact/ShippingConsumerTest.java
+++ orders/src/test/java/pact/ShippingConsumerTest.java
@@ -1,52 +1,44 @@
 package pact;
 
-import au.com.dius.pact.consumer.*;
-import org.junit.Assert;
-import org.junit.Rule;
-import org.junit.Test;
-
-import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
-import au.com.dius.pact.model.v3.messaging.MessagePact;
+import au.com.dius.pact.consumer.ConsumerPactTestMk2;
+import au.com.dius.pact.consumer.MockServer;
 import au.com.dius.pact.consumer.PactProviderRuleMk2;
-import au.com.dius.pact.consumer.ConsumerPactTest;
-
-
-import au.com.dius.pact.consumer.Pact;
-import au.com.dius.pact.consumer.PactVerification;
-import au.com.dius.pact.consumer.dsl.PactDslRequestWithoutPath;
-import au.com.dius.pact.consumer.dsl.PactDslResponse;
+import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.model.RequestResponsePact;
-
+import org.junit.Rule;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
 
 public class ShippingConsumerTest extends ConsumerPactTestMk2  {
 
     @Rule
     public PactProviderRuleMk2  rule = new PactProviderRuleMk2 ("shipping-service", "localhost", 9999, this);
 
-    @Pact(provider = "shipping-server", consumer = "orders-service")
-    public MessagePact createPact(MessagePactBuilder builder) {
+    @Override
+    protected RequestResponsePact createPact(PactDslWithProvider builder) {
+
         PactDslJsonBody body = new PactDslJsonBody().stringValue("name", "shipment1").stringValue("amount", "8.0");
 
-//            Map<String, String> metadata = new HashMap<String, String>();
-//            metadata.put("contentType", "application/json");
-
-            return builder.given("shippmeng of order sent.")
-                    .expectsToReceive("success")
-                    .withContent(body)
-                    .toPact();
-    }
-
-        @Test
-        @PactVerification({"test_provider", "SomeProviderState"})
-        public void test() throws Exception {
-//            Assert.assertNotNull(new String(currentMessage));
-        }
-
-    @Override
-    protected RequestResponsePact createPact(PactDslWithProvider pactDslWithProvider) {
-        return null;
+        return builder.given("shippment of order sent.")
+                .uponReceiving("shippment request from order service")
+                .path("/")
+                .method("GET")
+                .willRespondWith()
+                .status(200)
+                .body("{\"responsetest\": true, \"name\": \"harry\"}")
+                .given("test state 2") // NOTE: Using provider states are optional, you can leave it out
+                .uponReceiving("ExampleJavaConsumerPactTest second test interaction")
+                .method("OPTIONS")
+                .path("/second")
+                .body("")
+                .willRespondWith()
+                .status(200)
+                .body("")
+                .toPact();
     }
 
     @Override
@@ -61,10 +53,11 @@ public class ShippingConsumerTest extends ConsumerPactTestMk2  {
 
     @Override
     protected void runTest(MockServer mockServer) throws IOException {
-
+        assertEquals(new ConsumerClient(mockServer.getUrl()).options("/second"), 200);
+        Map expectedResponse = new HashMap();
+        expectedResponse.put("responsetest", true);
+        expectedResponse.put("name", "harry");
+        assertEquals(new ConsumerClient(mockServer.getUrl()).getAsMap("/", ""), expectedResponse);
+        assertEquals(new ConsumerClient(mockServer.getUrl()).options("/second"), 200);
     }
-//
-//        public void setMessage(byte[] messageContents) {
-//            currentMessage = messageContents;
-//        }
 }
