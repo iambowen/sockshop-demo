da85cf72a468b9f1f18a66f61609f484e89edd94
Bowen Ma <iambowen.m@gmail.com>
2018-03-17 21:32:40 +0800

    update carts docker-compose

--- carts/carts.sh
+++ carts/carts.sh
@@ -5,6 +5,3 @@ cp /etc/resolv.conf /tmp
 sed -i s/"^.*search.*$"/"search"/g /tmp/resolv.conf
 cat /tmp/resolv.conf > /etc/resolv.conf
 java -jar carts.jar
-
-~                                                                                                                                                                        
-~       
